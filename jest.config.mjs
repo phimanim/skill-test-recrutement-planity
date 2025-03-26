2ddcb2c219d68856c6f73d7b0f643101e2123ea4
phimanim <aliker.imane@gmail.com>
2025-03-26 09:29:34 +0100

    Setup Jest

---
+++ jest.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "\\.(css)$": "<rootDir>/__mocks__/styleMock.js",
+  },
+};
